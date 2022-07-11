8b8b577f2acc9d6e97da703e44b1f7cbf2d92f91
Parkseonho <tjsgh2103@gmail.com>
2022-07-11 18:22:39 +0900

    검색

--- src/main/java/main/Board.java
+++ src/main/java/main/Board.java
@@ -3,15 +3,12 @@ package main;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
-
 public class Board {
     ArrayList<Article> articles = new ArrayList<>();
     Scanner in = new Scanner(System.in);
 
     public void run(){
 
-        /* test date를 제작하여 여러개의 list를 확인해보자 */
         makeTestDate();
 
         while(true) {
@@ -26,9 +23,12 @@ public class Board {
             } else if (cmd.equals("add")) {
                 addArticle();
             } else if (cmd.equals("list")) {
-                printArticles();
+                printArticles(articles);
             } else if (cmd.equals("update")) {
                 updateArticle();
+                printArticles(articles);
+            }else if(cmd.equals("search")){
+                searchArticle();
             }else {
                 System.out.println("알 수 없는 명령어");
             }
@@ -36,7 +36,29 @@ public class Board {
         }
     }
 
-    /* test date 제작 후 articles ArrayList에 추가 */
+    private void searchArticle() {
+        /*
+         - 입력한 키워드가 들어간 게시물을 찾는것임
+         - Article을 가지는 searchArticleList라는 ArrayList 생성
+         - 게시물 0부터 articles 개수만큼 반복하면서
+           n번째 article의 제목에 keyword가 포함되어 있으면
+           searchArticleList에 n번째 articles 게시물 더하기
+           * contains(문자열) : 이 문자열 내에서 지정한 문자가 포함되어 있는지 여부 확인
+         - printArticles에 searchArticleList를 넣어 원하는것만 출력할수 있게 함
+         */
+        System.out.print("검색 키워드를 입력해주세요 : ");
+        String keyword = in.nextLine();
+
+        ArrayList<Article> searchArticleList = new ArrayList<>();
+        for(int i = 0; i < articles.size(); i++){
+            if(articles.get(i).title.contains(keyword)){
+                searchArticleList.add(articles.get(i));
+            }
+        }
+        printArticles(searchArticleList);
+    }
+
+
     private void makeTestDate() {
         Article test1 = new Article("제목1","내용1");
         Article test2 = new Article("제목2","내용2");
@@ -70,7 +92,7 @@ public class Board {
 
         System.out.println("수정 완료");
 
-        printArticles();
+
     }
 
 
@@ -93,10 +115,15 @@ public class Board {
         System.out.println("add - 게시물 등록");
         System.out.println("list - 게시물 목록");
         System.out.println("update - 게시물 수정");
+        System.out.println("search - 검색");
         System.out.println("exit - 프로그램 종료");
     }
 
-    public void printArticles() {
+        /*
+        - search가 있을때는 searchArticleList에 있는 articles를 출력
+        - search가 없을때는 articles 전부 출력
+        */
+    public void printArticles(ArrayList<Article> articles) {
 
         for (int i = 0; i < articles.size(); i++) {
             Article article = articles.get(i);
---
+++ src/main/java/main/Test.java
@@ -0,0 +1,11 @@
+package main;
+
+
+/* contains test */
+public class Test {
+    public static void main(String[] args) {
+        String str1 = "안녕하세요";
+
+        System.out.println(str1.contains("안녕"));
+    }
+}
